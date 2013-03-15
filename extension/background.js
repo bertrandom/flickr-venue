374bd8b27d4146dde464e4f359d03fc48dcd987b
Bertrand Fan <bertrand@fan.net>
2013-03-14 19:39:07 -0700

    tabs to spaces

--- extension/background.js
+++ extension/background.js
@@ -2,22 +2,22 @@ var pages = {}
 var selectedTabId = null;
 
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-    pages[sender.tab.id] = message;
-    chrome.pageAction.show(sender.tab.id);
+  pages[sender.tab.id] = message;
+  chrome.pageAction.show(sender.tab.id);
 });
 
 chrome.tabs.onActivated.addListener(function(activeInfo) {
-	selectedTabId = activeInfo.tabId;
+  selectedTabId = activeInfo.tabId;
 });
 
 getData = function() {
-	return pages[selectedTabId];
+  return pages[selectedTabId];
 }
 
 setVenue = function(foursquare_venue_id) {
-	pages[selectedTabId]['foursquare_venue_id'] = foursquare_venue_id;
+  pages[selectedTabId]['foursquare_venue_id'] = foursquare_venue_id;
 }
 
 removeVenue = function() {
-	delete pages[selectedTabId]['foursquare_venue_id'];
+  delete pages[selectedTabId]['foursquare_venue_id'];
 }
--- extension/content_script.js
+++ extension/content_script.js
@@ -1,98 +1,98 @@
 $(function() {
 
-	var page_type = $('meta[property="og:type"]');
-	if (page_type && page_type.attr('content') == 'flickr_photos:photo') {
+  var page_type = $('meta[property="og:type"]');
+  if (page_type && page_type.attr('content') == 'flickr_photos:photo') {
 
-		// Quick hack to determine if it's your photo
-		var privacy_toggle = document.getElementById('edit-privacy-collapsed');
-		if (!privacy_toggle) {
-			return;
-		}
+    // Quick hack to determine if it's your photo
+    var privacy_toggle = document.getElementById('edit-privacy-collapsed');
+    if (!privacy_toggle) {
+      return;
+    }
 
-		var metas = document.querySelectorAll("meta");
+    var metas = document.querySelectorAll("meta");
 
-		var latitude = null;
-		var longitude = null;
+    var latitude = null;
+    var longitude = null;
 
-		var meta_latitude = $('meta[property="flickr_photos:location:latitude"]');
-		var meta_longitude = $('meta[property="flickr_photos:location:longitude"]');
+    var meta_latitude = $('meta[property="flickr_photos:location:latitude"]');
+    var meta_longitude = $('meta[property="flickr_photos:location:longitude"]');
 
-		if (meta_latitude) {
-			latitude = meta_latitude.attr('content');
-		}
+    if (meta_latitude) {
+      latitude = meta_latitude.attr('content');
+    }
 
-		if (meta_longitude) {
-			longitude = meta_longitude.attr('content');
-		}
+    if (meta_longitude) {
+      longitude = meta_longitude.attr('content');
+    }
 
-		if (!latitude) {
-			return;		
-		}
+    if (!latitude) {
+      return;   
+    }
 
-		var data = {geo: {lat: latitude, lon: longitude}}
+    var data = {geo: {lat: latitude, lon: longitude}}
 
-		var api_key = null;
-		var secret = null;
+    var api_key = null;
+    var secret = null;
 
-		$('script').each(function(index, el) {
+    $('script').each(function(index, el) {
 
-			var re = /"api_key":"([0-9a-z]*)"/
-			var content = $(el).html();
-			var result = content.match(re);
+      var re = /"api_key":"([0-9a-z]*)"/
+      var content = $(el).html();
+      var result = content.match(re);
 
-			if (result && result.length >= 2) {
-				api_key = result[1];
-			}
+      if (result && result.length >= 2) {
+        api_key = result[1];
+      }
 
-			var re = /"secret":"([0-9a-z]*)"/
-			var content = $(el).html();
-			var result = content.match(re);
+      var re = /"secret":"([0-9a-z]*)"/
+      var content = $(el).html();
+      var result = content.match(re);
 
-			if (result && result.length >= 2) {
-				secret = result[1];
-			}
+      if (result && result.length >= 2) {
+        secret = result[1];
+      }
 
-		});
+    });
 
-		if (api_key) {
-			data['api_key'] = api_key;
-		}
+    if (api_key) {
+      data['api_key'] = api_key;
+    }
 
-		if (secret) {
-			data['secret'] = secret;
-		}
+    if (secret) {
+      data['secret'] = secret;
+    }
 
-		var magic_cookie_input = $('input[name="magic_cookie"]');
+    var magic_cookie_input = $('input[name="magic_cookie"]');
 
-		if (magic_cookie_input) {
-			data['auth_hash'] = magic_cookie_input.val();
-		}
+    if (magic_cookie_input) {
+      data['auth_hash'] = magic_cookie_input.val();
+    }
 
-		var photo_input = $('input[name="photo"]');
+    var photo_input = $('input[name="photo"]');
 
-		if (photo_input) {
-			data['id'] = photo_input.val();
-		}
+    if (photo_input) {
+      data['id'] = photo_input.val();
+    }
 
-		var foursquare_venue_id = null;
+    var foursquare_venue_id = null;
 
-		$('a.f-machinetag-sprite-link').each(function(index, el) {
+    $('a.f-machinetag-sprite-link').each(function(index, el) {
 
-			var re = /foursquare.com\/venue\/([0-9a-z]*)/
-			var result = el.href.match(re);
-			if (result && result.length >= 2) {
-				foursquare_venue_id = result[1];
-			}
+      var re = /foursquare.com\/venue\/([0-9a-z]*)/
+      var result = el.href.match(re);
+      if (result && result.length >= 2) {
+        foursquare_venue_id = result[1];
+      }
 
-		});
+    });
 
-		if (foursquare_venue_id) {
-			data['foursquare_venue_id'] = foursquare_venue_id;
-		}
+    if (foursquare_venue_id) {
+      data['foursquare_venue_id'] = foursquare_venue_id;
+    }
 
-		chrome.extension.sendMessage(data, function(response) {
-		});
+    chrome.extension.sendMessage(data, function(response) {
+    });
 
-	}
+  }
 
 });
